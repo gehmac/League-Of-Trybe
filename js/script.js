22d943a3637f4ca69fc4941bd829673ebb0c06b3
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-28 20:03:55 -0300

    apresentaçao das informaçoes do campeao funcionando ok

--- js/script.js
+++ js/script.js
@@ -3,42 +3,38 @@ const sectionRigth = document.querySelector('#rigth')
 const championsHTML = document.querySelector('.champions');
 
 const getChampionInfo = async (championName) => {
-  const champion = await fetch(`https://ddragon.leagueoflegends.com/cdn/11.19.1/data/pt_BR/champion/${championName}.json`);
-  const convertedChampion = await champion.json();
-  // console.log(convertedChampion);
-  const objChampion = {
-    name: convertedChampion.name,
-    title: convertedChampion.title,
-    
-  }
-  return objChampion;
+  const result = await fetch(`https://ddragon.leagueoflegends.com/cdn/11.19.1/data/pt_BR/champion/${championName}.json`);
+  const convertedResult = await result.json();
+  const championObject = Object.values(convertedResult.data)[0];
+  return championObject;
 }
-getChampionInfo('Aatrox');
 
-const showInfo = ({ data }) => {
-  const championData = Object.values(data)[0];
+const showInfo = (championObject) => {
+  console.log(championObject);
   const nameSection = document.querySelector('#champion-name');
-  const loreSection = document.querySelector('#lore');
-  const classSection = document.querySelector('#class');
+  const titleSection = document.querySelector('#champion-title');
+  const loreSection = document.querySelector('#champion-lore');
+  const classSection = document.querySelector('#champion-class');
   const championImg = document.querySelector('#champion-img');
+  const skinsAmount = championObject.skins.length;
 
-  nameSection.innerText = championData.name;
-  loreSection.innerText = championData.lore;
-  classSection.innerText = championData.info;
-  championImg.src = 
-}
+  nameSection.innerText = championObject.name;
+  titleSection.innerText = championObject.title;
+  loreSection.innerText = championObject.lore;
+  classSection.innerText = championObject.tags;
+  championImg.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championObject.name}_0.jpg`
+};
 
-const handleChampionClick = (event) => {
+const handleChampionClick = async (event) => {
   const previousSelected = document.querySelector('.selected-champion')
   const championName = event.target.id
   if (!previousSelected) {
     // changeDisplay();
-  }
-  else {
+  } else {
     previousSelected.classList.remove('selected-champion');
   }
   event.target.classList.add('selected-champion');
-  const championInfo = getChampionInfo(championName);
+  const championInfo = await getChampionInfo(championName);
   showInfo(championInfo);
 }
 
